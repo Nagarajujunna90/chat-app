8c2df47fb6a2ba08c897056c0967322071747614
njunna <nagarajujunna90@gmail.com>
2023-09-28 19:11:07 +0530

    Added AppSpec.yml file for codeDeploy

---
+++ scripts/server_stop.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#sudo pkill -f 'java -jar'
+
+# Kill Java process
+
+# Determine the pid
+echo "killing process starts"
+PID=`ps -C java -o pid=`
+
+if [ -n "$PID" ]; then
+    echo "killing process"
+    kill -9 $PID
+else
+    echo "empty"
+fi
+
+cd /opt
+rm -rf root
