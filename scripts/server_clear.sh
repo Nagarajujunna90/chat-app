808617aab20810008a93e9f0625a0d6813b73db1
njunna <nagarajujunna90@gmail.com>
2023-09-29 09:53:55 +0530

    updated with conditions

--- scripts/server_clear.sh
+++ scripts/server_clear.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
-sudo mkdir "chat-app"
-#sudo rm -rf /home/ec2-user/chat-app
-
+#sudo mkdir "chat-app"
+##sudo rm -rf /home/ec2-user/chat-app
+directory="/home/ec2-user/chat-app"
+if [ ! -d "$directory" ]; then
+    sudo mkdir -p "$directory"
+    echo "Directory '$directory' created."
+else
+    sudo echo "Directory '$directory' already exists."
+fi
