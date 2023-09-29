ce2539a8776c6b3fb0705fa088703ac91f3e1df9
njunna <nagarajujunna90@gmail.com>
2023-09-29 08:10:42 +0530

    changed port

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -2,9 +2,11 @@
 cd /home/ec2-user/chat-app
 #sudo unzip spring-build-demo.zip
 mvn clean package
-mvn spring-boot:run -Drun.jvmArguments='-Dserver.port=9091' > /dev/null 2> /dev/null < /dev/null &
+#mvn spring-boot:run -Drun.jvmArguments='-Dserver.port=9091' > /dev/null 2> /dev/null < /dev/null &
 sudo java -jar target/spring-boot-sample-app-1.0.0.jar \
     *.jar > /dev/null 2> /dev/null < /dev/null &
+
+
 #sudo kill $(sudo lsof -t -i:9091)
 #sudo java -jar chat-app-1.0.0.jar \
 #    *.jar > /dev/null 2> /dev/null < /dev/null &
