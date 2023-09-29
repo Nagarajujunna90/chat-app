3921a33a6e710b400c2a1dd700788a432aa03a48
njunna <nagarajujunna90@gmail.com>
2023-09-29 08:55:31 +0530

    updated jar name

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -3,7 +3,7 @@ cd /home/ec2-user/chat-app
 #sudo unzip spring-build-demo.zip
 mvn clean package
 #mvn spring-boot:run -Drun.jvmArguments='-Dserver.port=9091' > /dev/null 2> /dev/null < /dev/null &
-sudo java -jar target/spring-boot-sample-app-1.0.0.jar \
+sudo java -jar target/chat-app-0.0.1-SNAPSHOT.jar \
     *.jar > /dev/null 2> /dev/null < /dev/null &
 
 
