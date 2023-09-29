4750fab92ad880f728c872c0b6ec73967ddb1ed0
njunna <nagarajujunna90@gmail.com>
2023-09-29 07:40:00 +0530

    changed folder name

--- scripts/fix_privileges.sh
+++ scripts/fix_privileges.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-sudo chmod +x /home/ec2-user/server/*
+sudo chmod +x /home/ec2-user/chat-app/*
 
 #chmod +x /home/ec2-user/server/* jar
 #chmod +x /home/ec2-user/server/server_start.sh
