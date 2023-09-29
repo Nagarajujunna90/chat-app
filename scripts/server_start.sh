50fd573016886c91c3f43648979dce1633c562cb
njunna <nagarajujunna90@gmail.com>
2023-09-29 10:04:20 +0530

    updated

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/chat-app
-#sudo unzip spring-build-demo.zip
-mvn clean package
-#mvn spring-boot:run -Drun.jvmArguments='-Dserver.port=9091' > /dev/null 2> /dev/null < /dev/null &
+sudo mvn clean install
 sudo java -jar target/chat-app-0.0.1-SNAPSHOT.jar \
     *.jar > /dev/null 2> /dev/null < /dev/null &
+
+#sudo unzip spring-build-demo.zip
+#mvn spring-boot:run -Drun.jvmArguments='-Dserver.port=9091' > /dev/null 2> /dev/null < /dev/null &
 #sudo java -jar -Dserver.port=9091 \
 #    *.jar > /dev/null 2> /dev/null < /dev/null &
 
